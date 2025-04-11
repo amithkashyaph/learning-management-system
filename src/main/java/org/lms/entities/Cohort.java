3d8c2984f4f2c4154fa4f7984041ffacedc62573
amithkashyap <amithkashyaprv@gmail.com>
2025-04-11 23:34:27 +0530

    updated cohort entity

--- src/main/java/org/lms/entities/Cohort.java
+++ src/main/java/org/lms/entities/Cohort.java
@@ -1,4 +1,12 @@
 package org.lms.entities;
 
+import java.util.Date;
+
 public class Cohort {
+    private int cohortId;
+    private String cohortName;
+    private String cohortDescription;
+    private Date startDate;
+    private Date endDate;
+
 }
