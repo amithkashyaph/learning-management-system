ab3f6673107169553551fa65342092b1db18f8d2
amithkashyap <amithkashyaprv@gmail.com>
2025-04-12 23:32:32 +0530

    updated course entity

--- src/main/java/org/lms/entities/Course.java
+++ src/main/java/org/lms/entities/Course.java
@@ -1,4 +1,33 @@
 package org.lms.entities;
 
+import org.lms.enums.CourseLanguage;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
+    private int courseId;
+    private String courseName;
+    private String courseDescription;
+    private CourseLanguage courseLanguage;
+    private List<Cohort> cohorts;
+
+    public Course(int courseId, String courseName, String courseDescription, CourseLanguage courseLanguage) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.courseDescription = courseDescription;
+        this.courseLanguage = courseLanguage;
+        this.cohorts = new ArrayList<>();
+    }
+
+    public void displayCourseDetails() {
+        System.out.println("Course Id: " + courseId);
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Course Description: " + courseDescription);
+        System.out.println("Course Language: " + courseLanguage);
+        for(Cohort cohort: cohorts) {
+            System.out.println(cohort);
+        }
+    }
+
 }
