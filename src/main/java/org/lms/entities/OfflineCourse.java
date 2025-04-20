ea49864836280178737aed16b6d8c4cd72e6c629
amithkashyap <amithkashyaprv@gmail.com>
2025-04-20 18:14:02 +0530

    updated offline course class

--- src/main/java/org/lms/entities/OfflineCourse.java
+++ src/main/java/org/lms/entities/OfflineCourse.java
@@ -3,4 +3,11 @@ package org.lms.entities;
 import org.lms.enums.CourseLanguage;
 
 public class OfflineCourse extends Course {
+
+    private String location;
+
+    public OfflineCourse(int courseId, String courseName, String courseDescription, CourseLanguage courseLanguage, String location) {
+        super(courseId, courseName, courseDescription, courseLanguage);
+        this.location = location;
+    }
 }
