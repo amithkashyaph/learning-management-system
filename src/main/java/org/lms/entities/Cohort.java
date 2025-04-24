f0e88f3fff2206d99468ab9ef368bdeaca30fdad
amithkashyap <amithkashyaprv@gmail.com>
2025-04-25 00:04:17 +0530

    updated cohort entity

--- src/main/java/org/lms/entities/Cohort.java
+++ src/main/java/org/lms/entities/Cohort.java
@@ -14,4 +14,59 @@ public class Cohort {
 
     private List<Instructor> instructors;
 
+    public int getCohortId() {
+        return cohortId;
+    }
+
+    public void setCohortId(int cohortId) {
+        this.cohortId = cohortId;
+    }
+
+    public String getCohortName() {
+        return cohortName;
+    }
+
+    public void setCohortName(String cohortName) {
+        this.cohortName = cohortName;
+    }
+
+    public String getCohortDescription() {
+        return cohortDescription;
+    }
+
+    public void setCohortDescription(String cohortDescription) {
+        this.cohortDescription = cohortDescription;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<Learner> getLearners() {
+        return learners;
+    }
+
+    public void setLearners(List<Learner> learners) {
+        this.learners = learners;
+    }
+
+    public List<Instructor> getInstructors() {
+        return instructors;
+    }
+
+    public void setInstructors(List<Instructor> instructors) {
+        this.instructors = instructors;
+    }
 }
